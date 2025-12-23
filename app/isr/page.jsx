d1f90bc54bddb4e487897243437b5f4f4a95d37d
jitenpanchal1 <jeetenpanchal84@gmail.com>
2025-12-23 14:35:09 +0530

    update

--- app/isr/page.jsx
+++ app/isr/page.jsx
@@ -1,25 +1,28 @@
-export default async function Page() {
-  const res = await fetch(`${process.env.API_BASE_URL}/manue`, {
-    next: { revalidate: 100 } // ISR
-  });
 
-  if (!res.ok) {
-    throw new Error("Failed to fetch menu");
-  }
 
-  const data = await res.json();      // object returned by backend
-  const menu = data.manue;            // extract the array
+export default async function page() {
 
-  return (
-    <div>
-      <h1>Incremental Static Regeneration Demo 🚀</h1>
-      <ul>
-        {menu.map(item => (
-          <li key={item.id}>
-            {item.name} — ₹{item.price}
-          </li>
-        ))}
-      </ul>
-    </div>
-  );
+    const Api = await fetch("http://localhost:5000/manue", {
+        next: { revalidate: 100 }
+    })
+    const read = await Api.json()
+    console.log("Run manue",)
+
+    return (
+        <div>
+            <h1>increamanetal</h1>
+            <div>
+                <h1>Incremental Static Regeneration Demo 🚀</h1>
+                <ul>
+                    {read.map((item) => (
+                        <li key={item.id}>
+                            {item.name} — ₹{item.price}
+                        </li>
+                    ))}
+                </ul>
+            </div>
+            <div>
+            </div>
+        </div>
+    )
 }
